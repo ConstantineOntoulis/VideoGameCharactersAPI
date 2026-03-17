59c2a7d31cac94028c9a042d594f6f8bfb6ac91b
Constantine Ontoulis <konstantinosontoulis@gmail.com>
2026-03-17 19:12:49 +0200

    feat(services): update service contract to use CreateCharacterRequest and UpdateCharacterRequest

--- Services/IVideoGameCharacterService.cs
+++ Services/IVideoGameCharacterService.cs
@@ -8,8 +8,8 @@ namespace VideoGameCharactersAPI.Services
         //Add CRUD Methods within the interface
         Task<List<CharacterResponseDto>> GetAllCharactersAsync();
         Task<CharacterResponseDto?> GetCharacterByIdAsync(int id);
-        Task<CharacterResponseDto> AddCharacterAsync(Character character);
-        Task<bool> UpdateCharacterAsync(int id, Character character);
+        Task<CharacterResponseDto> AddCharacterAsync(CreateCharacterRequest character);
+        Task<bool> UpdateCharacterAsync(int id, UpdateCharacterRequest character);
         Task<bool> DeleteCharacterAsynce(int id);
     }
 }
