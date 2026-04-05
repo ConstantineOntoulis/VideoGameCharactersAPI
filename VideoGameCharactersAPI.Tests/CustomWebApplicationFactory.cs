0779349e02c30d45942e8fa25768554632f7f9e1
Constantine Ontoulis <konstantinosontoulis@gmail.com>
2026-04-05 15:37:18 +0300

    test: add custom WebApplicationFactory for integration tests

---
+++ VideoGameCharactersAPI.Tests/CustomWebApplicationFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+// This is the reusable object that boots the app for integration tests
+namespace VideoGameCharactersAPI.Tests
+{
+    public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+    {
+    }
+}
