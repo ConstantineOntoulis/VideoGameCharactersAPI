3af44e5edffb53254285b98d1f33a7571f5be276
Constantine Ontoulis <konstantinosontoulis@gmail.com>
2026-04-25 17:07:35 +0300

    ci: assemply cleanup

--- VideoGameCharactersAPI.Tests/AssemblyInfo.cs
+++ VideoGameCharactersAPI.Tests/AssemblyInfo.cs
@@ -1,11 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace VideoGameCharactersAPI.Tests
-{
-    internal class AssemblyInfo
-    {
-        [assembly: Xunit.CollectionBehavior(DisableTestParallelization = true)];
-    }
-}
+﻿[assembly: Xunit.CollectionBehavior(DisableTestParallelization = true)]
