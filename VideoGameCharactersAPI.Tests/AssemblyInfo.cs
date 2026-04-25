c46539fc879bba64a53ea943fc52a254872535ce
Constantine Ontoulis <konstantinosontoulis@gmail.com>
2026-04-25 17:04:37 +0300

    ci: stabilize integration tests by configuring the test host and disabling parallel execution

---
+++ VideoGameCharactersAPI.Tests/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VideoGameCharactersAPI.Tests
+{
+    internal class AssemblyInfo
+    {
+        [assembly: Xunit.CollectionBehavior(DisableTestParallelization = true)];
+    }
+}
--- VideoGameCharactersAPI.Tests/CustomWebApplicationFactory.cs
+++ VideoGameCharactersAPI.Tests/CustomWebApplicationFactory.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 
-// This is the reusable object that boots the app for integration tests
-namespace VideoGameCharactersAPI.Tests
+namespace VideoGameCharactersAPI.Tests;
+
+public class CustomWebApplicationFactory : WebApplicationFactory<Program>
 {
-    public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
+        builder.UseEnvironment("Testing");
     }
 }
